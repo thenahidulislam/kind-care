f876cae85433095b100c2d276f214d024b436a25
Nahidul Islam <nahidulislam1417@gmail.com>
2024-09-24 14:47:38 +0600

    Javascript functionality added with history of donation.

--- js/addmoney.js
+++ js/addmoney.js
@@ -1,4 +1,4 @@
-//add money by id
+//shared function to add money by id
 //id=input ammount of each section
 function addMoneyFromSections(id){
     const inputValue = document.getElementById(id).value;
@@ -7,7 +7,7 @@ function addMoneyFromSections(id){
     return donationValue;
 }
 
-//show this money in section balance
+//shared function to show this money in section balance
 //id = show section balance in the top of the texts
 function SectionBalance(id){
     const gettingPrevSectionBalance = document.getElementById(id).innerText;
@@ -15,43 +15,87 @@ function SectionBalance(id){
     return prevSectionBalance;
 }
 
+//Get main balance value in number
+
+
 //adding fund in Noakhali Donation
-const sectionTotalDonationNoakhali = document.getElementById('noakhali-donate-now-btn').addEventListener('click',function(){
+const sectionTotalDonationNoakhali = document.getElementById('noakhali-donate-now-btn').addEventListener('click', function(){
+
+    const mainBalance = SectionBalance('main-balance');
     const noakhaliNewDonation = addMoneyFromSections('input-noakhali-donation');
+    //validation
+    if (isNaN(noakhaliNewDonation) || noakhaliNewDonation > mainBalance || noakhaliNewDonation <=0 ) {
+        alert("Please enter a valid ammount in numeric");
+        return;
+    }
+
     const noakhaliSectionBalance = SectionBalance('noakhali-donation-balance');
     const noakhaliNewBalance = noakhaliSectionBalance + noakhaliNewDonation;
     const showSectionBalance = document.getElementById('noakhali-donation-balance').innerText = noakhaliNewBalance;
 
     //decrease in main balance after donation in any section
-    const mainBalance = SectionBalance('main-balance');
+    // const mainBalance = SectionBalance('main-balance');
     const newMainBalance = mainBalance - noakhaliNewDonation;
     const showNewMainBalance = document.getElementById('main-balance').innerText = newMainBalance;
+
+    //history
+    const div = document.createElement('div');
+    const date = new Date();
+    div.innerHTML = `
+    <p class="text-xl m-2 border-2 border-neutral-500 rounded px-3"> ${noakhaliNewDonation} was Donated at ${date} in the Noakhali Flood Donation.</p>
+    `
+    document.getElementById('history-items').appendChild(div);
     })
 
 //adding fund in Feni Donation
-const sectionTotalDonationFeni = document.getElementById('noakhali-donate-now-btn').addEventListener('click',function(){
-    const noakhaliNewDonation = addMoneyFromSections('input-noakhali-donation');
-    const noakhaliSectionBalance = SectionBalance('noakhali-donation-balance');
-    const noakhaliNewBalance = noakhaliSectionBalance + noakhaliNewDonation;
-    const showSectionBalance = document.getElementById('noakhali-donation-balance').innerText = noakhaliNewBalance;
+const sectionTotalDonationFeni = document.getElementById('feni-donate-now-btn').addEventListener('click',function(){
+    const mainBalance = SectionBalance('main-balance');
+    const feniNewDonation = addMoneyFromSections('input-feni-donation');
+    //validation
+    if (isNaN(feniNewDonation) || feniNewDonation > mainBalance || feniNewDonation <=0 ) {
+        alert("Please enter a valid ammount in numeric");
+        return;
+    }
+
+    const feniSectionBalance = SectionBalance('feni-donation-balance');
+    const feniNewBalance = feniSectionBalance + feniNewDonation;
+    const showSectionBalance = document.getElementById('feni-donation-balance').innerText = feniNewBalance;
 
     //decrease in main balance after donation in any section
-    const mainBalance = SectionBalance('main-balance');
-    const newMainBalance = mainBalance - noakhaliNewDonation;
+    const newMainBalance = mainBalance - feniNewDonation;
     const showNewMainBalance = document.getElementById('main-balance').innerText = newMainBalance;
+    //history
+    const div = document.createElement('div');
+    const date = new Date();
+    div.innerHTML = `
+    <p class="text-xl m-2 border-2 border-neutral-500 rounded px-3"> ${feniNewDonation} was Donated at ${date} in the Feni Flood Donation.</p>
+    `
+    document.getElementById('history-items').appendChild(div);
     })
 
 //adding fund in Qouta Donation
-const sectionTotalDonationQouta = document.getElementById('noakhali-donate-now-btn').addEventListener('click',function(){
-    const noakhaliNewDonation = addMoneyFromSections('input-noakhali-donation');
-    const noakhaliSectionBalance = SectionBalance('noakhali-donation-balance');
-    const noakhaliNewBalance = noakhaliSectionBalance + noakhaliNewDonation;
-    const showSectionBalance = document.getElementById('noakhali-donation-balance').innerText = noakhaliNewBalance;
+const sectionTotalDonationQouta = document.getElementById('qouta-donate-now-btn').addEventListener('click', function(){
+    const mainBalance = SectionBalance('main-balance');
+    const qoutaNewDonation = addMoneyFromSections('input-qouta-donation');
+    //validation
+    if (isNaN(qoutaNewDonation) || qoutaNewDonation > mainBalance || qoutaNewDonation <=0 ) {
+        alert("Please enter a valid ammount in numeric");
+        return;
+    }
+    const qoutaSectionBalance = SectionBalance('qouta-donation-balance');
+    const qoutaNewBalance = qoutaSectionBalance + qoutaNewDonation;
+    const showSectionBalance = document.getElementById('qouta-donation-balance').innerText = qoutaNewBalance;
 
     //decrease in main balance after donation in any section
-    const mainBalance = SectionBalance('main-balance');
-    const newMainBalance = mainBalance - noakhaliNewDonation;
+    const newMainBalance = mainBalance - qoutaNewDonation;
     const showNewMainBalance = document.getElementById('main-balance').innerText = newMainBalance;
+
+    const div = document.createElement('div');
+    const date = new Date();
+    div.innerHTML = `
+    <p class="text-xl m-2 border-2 border-neutral-500 rounded px-3"> ${qoutaNewDonation} was Donated at ${date} for Injured in the Quota Movement.</p>
+    `
+    document.getElementById('history-items').appendChild(div);
     })
 
 
